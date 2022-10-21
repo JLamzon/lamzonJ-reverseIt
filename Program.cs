03e98cc1ce8d4c8ab01636f3aa5cc89a6adaa438
JLamzon <jlamzon@yahoo.com>
2022-10-21 10:39:45 -0700

    added peer review to program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 //10/19/22
 //GitHubChallenge - Redo mini Challenges
 //Reverse it - data validation and play again
+//Peer Review: Jasmine Leek - great program! Works well under all conditions including special characters
+//I especially liked how clear the output is for the user
 
 
 Console.Clear();
